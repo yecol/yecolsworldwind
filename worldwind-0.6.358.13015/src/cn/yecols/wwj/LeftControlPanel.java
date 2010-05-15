14ebb4a1fc5f02090bc676202777a39b0f8a10b6
xujb1987 <xujb1987@bb250d73-9378-718d-f2fc-8de336f2c953>
2010-05-15 13:56:42 +0000

    2010.5.15.Learning

--- worldwind-0.6.358.13015/src/cn/yecols/wwj/LeftControlPanel.java
+++ worldwind-0.6.358.13015/src/cn/yecols/wwj/LeftControlPanel.java
@@ -2,19 +2,25 @@ package cn.yecols.wwj;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import org.jgrapht.UndirectedGraph;
 import org.jgrapht.WeightedGraph;
+import org.jgrapht.alg.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultEdge;
 import org.xml.sax.SAXException;
 
 import gov.nasa.worldwind.WorldWindow;
+import gov.nasa.worldwind.geom.LatLon;
 import gov.nasa.worldwind.util.measure.MeasureTool;
+import gov.nasa.worldwind.util.measure.MeasureToolController;
 
 
 
@@ -23,14 +29,22 @@ public class LeftControlPanel extends JPanel {
 	private final MeasureTool measureTool;
 	
 	private WeightedGraph g;
+	private LatLon inputPathBegin;
+	private LatLon inputPathEnd;
 	
 	private JButton loadStreetsData;
+	private JButton startPosition;
+	private JButton computeShortestPath;
 
 	public LeftControlPanel(WorldWindow wwdObject, MeasureTool measureToolObject) {
-		super(new BorderLayout());
+		super(new FlowLayout());
 		this.wwd = wwdObject;
 		this.measureTool = measureToolObject;
 		this.makePanel(new Dimension(200, 300));
+		
+		//test shortest Path
+		inputPathBegin=new LatLon(30.2691,120.1021);
+		inputPathEnd=new LatLon(30.2412,120.1281);
 	}
 
 	private void makePanel(Dimension dimension) {
@@ -39,7 +53,7 @@ public class LeftControlPanel extends JPanel {
 		loadStreetsData.addActionListener(new ActionListener(){
 			 public void actionPerformed(ActionEvent event){
 				System.out.println("loading streets data");
-				StreetsDataReader streetsDataReader=new StreetsDataReader();
+				StreetsDataReader streetsDataReader=new StreetsDataReader(wwd);
 				try {
 					g=streetsDataReader.ReadStreetsData();
 					System.out.println(g.toString());
@@ -51,7 +65,37 @@ public class LeftControlPanel extends JPanel {
 			 }
 
 		});
+		
+		computeShortestPath=new JButton("»ñµÃÂ·¾¶");
+		computeShortestPath.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent event){
+				if(inputPathBegin==null||inputPathEnd==null)
+					System.out.println("Not input begin or end point yet.");
+				else if(g==null)
+					System.out.println("No graph builded yet.");
+				else{
+					DijkstraShortestPath shortestPath=new DijkstraShortestPath(g,inputPathBegin,inputPathEnd);
+					System.out.println(shortestPath.getPathLength());
+					System.out.println(shortestPath.getPathEdgeList().toString());
+					MeasureTool measureTool = new MeasureTool(wwd);
+					measureTool.setController(new MeasureToolController());
+					measureTool.setFollowTerrain(true);
+					measureTool.setMeasureShapeType(MeasureTool.SHAPE_PATH);
+					measureTool.setShowControlPoints(false);
+					measureTool.setArmed(true);
+					for(DefaultEdge e : (List<DefaultEdge>)shortestPath.getPathEdgeList()){
+						System.out.println(e.toString());
+					
+						
+					}
+				}
+					
+			}
+		});
+		
+		
 		add(loadStreetsData);
+		add(computeShortestPath);
 		
 	}
 
